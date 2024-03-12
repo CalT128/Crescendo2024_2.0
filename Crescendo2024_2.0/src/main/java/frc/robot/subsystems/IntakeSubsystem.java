6ac73a785d50106d9aa6309ec0de852b320b9fd6
Danny <92941727+CalT128@users.noreply.github.com>
2024-03-12 13:59:59 -0400

    3/12

--- Crescendo2024_2.0/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ Crescendo2024_2.0/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ShooterPosition;
 
 
+
 public class IntakeSubsystem extends SubsystemBase {
   /** Creates a new IntakeSubsystem. */
   ShooterSubsystem m_shooter;
